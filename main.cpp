464f91583d9131172b10fa22bdd3d5e390311101
Drake Ford <jdf028@uark.edu>
2021-01-17 14:11:32 -0600

    Finished Homework 1

--- main.cpp
+++ main.cpp
@@ -7,8 +7,11 @@ void homework1()
     //int main(){
 
     //declare variables
-    float width, length, area, cost, totalPumpkinVolume;
-    int pumpkinsPerWidth, pumpkinsPerLength, totalPumpkins = 0;
+    float width, length, area, maintCost, totalPumpkinVolume, transportCost, grossProfit, netProfit;
+    int pumpkinsPerWidth, pumpkinsPerLength, totalPumpkins, totalTrips = 0;
+    const int trailerVol = 8 * 14 * 6;
+    const float pi = 3.14;
+    const int pumpkinSpace = 4;
 
     //Get user input for length and width
     cout << "Enter Width:";
@@ -22,19 +25,34 @@ void homework1()
     cout << "The area of the farm is: " << area << " sq ft" << endl;
 
     //Calculate cost and print
-    cost = area * 0.06;
-    cout << "The farm's maintenance cost is: $" << fixed << setprecision(2) << cost << endl;
+    maintCost = area * 0.06;
+    cout << "The farm's maintenance cost is: $" << fixed << setprecision(2) << maintCost << endl;
 
     //Calculate pumpkins that can fit
-    pumpkinsPerLength = length / 4;
-    pumpkinsPerWidth = width / 4;
+    pumpkinsPerLength = length / pumpkinSpace;
+    pumpkinsPerWidth = width / pumpkinSpace;
     totalPumpkins = pumpkinsPerLength * pumpkinsPerWidth;
     cout << "The farm's pumpkin capacity is: " << totalPumpkins << endl << endl;
 
     //Calculate total area of pumpkins and print to screen
-    totalPumpkinVolume = (4.0/3) * 3.14 * totalPumpkins;
+    totalPumpkinVolume = (4.0/3) * pi * totalPumpkins;
     cout << "The volume of all the pumpkins is: " << fixed << setprecision(2) << totalPumpkinVolume << " cu ft" << endl;
 
+    //Calculate total trips and print to screen
+    totalTrips = (totalPumpkinVolume / trailerVol) + 0.5;
+    cout << "The total trips to the market will be: " << totalTrips << endl;
+
+    //Calculate transportation cost and print to screen
+    transportCost = 33.63 * totalTrips;
+    cout << "The transportation cost is: $" << fixed << setprecision(2) << transportCost << endl;
+
+    //Calculate gross profit and print to screen
+    grossProfit = totalPumpkins * 5.25;
+    cout << "The farmer's gross profit is: $" << fixed << setprecision(2) << grossProfit << endl << endl;
+
+    //Calculate net profit and print to screen
+    netProfit = grossProfit - maintCost - transportCost;
+    cout << "The net profit is: $" << fixed << setprecision(2) << netProfit << endl;
 
     //return 0;
     //}
@@ -42,5 +60,6 @@ void homework1()
 
 int main() {
     homework1();
+
     return 0;
 }
